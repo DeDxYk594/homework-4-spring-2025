627db5c407b1c8932b00b0e32a6fb1041794f971
DeDxYk594 <zhugeo@mail.ru>
2025-05-15 01:45:00 +0300

    fix homework, works now

---
+++ dump_session.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+print("HHHHHHFDLKFJSDKFJKSLJDFKLJSDLKJFSDJFLSJDKLFJSKDLFJSDFJKL")
+
+options = Options()
+
+options.add_argument("--verbose")
+options.add_argument("user-data-dir=vkads-userdata")
+options.add_argument("profile-directory=vkads-profile")
+
+
+driver = webdriver.Chrome(options=options)
+
+input("click Enter to exit...")
+driver.quit()
--- hw/code/ui/fixtures.py
+++ hw/code/ui/fixtures.py
@@ -2,56 +2,55 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
 from ui.pages.base_page import BasePage
 from ui.pages.main_page import MainPage
+import json
+import time
 
 
 @pytest.fixture()
 def driver(config):
-    browser = config['browser']
-    url = config['url']
-    selenoid = config['selenoid']
-    vnc = config['vnc']
+    browser = config["browser"]
+    selenoid = config["selenoid"]
+    vnc = config["vnc"]
     options = Options()
     if selenoid:
         capabilities = {
-            'browserName': 'chrome',
-            'version': '118.0',
+            "browserName": "chrome",
+            "version": "118.0",
         }
         if vnc:
-            capabilities['enableVNC'] = True
+            capabilities["enableVNC"] = True
         driver = webdriver.Remote(
-            'http://127.0.0.1:4444/wd/hub',
+            "http://127.0.0.1:4444/wd/hub",
             options=options,
-            desired_capabilities=capabilities
+            desired_capabilities=capabilities,
         )
-    elif browser == 'chrome':
-        driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-    elif browser == 'firefox':
-        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    elif browser == "chrome":
+        options.add_argument("--user-data-dir=vkads_userdata")
+        options.add_argument("profile-directory=vkads-profile")
+        driver = webdriver.Chrome(options)
     else:
         raise RuntimeError(f'Unsupported browser: "{browser}"')
-    driver.get(url)
     driver.maximize_window()
+    driver.get("https://ads.vk.com")
+    assert 2 + 2 == 228
     yield driver
     driver.quit()
 
 
 def get_driver(browser_name):
-    if browser_name == 'chrome':
-        browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-    elif browser_name == 'firefox':
-        browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    if browser_name == "chrome":
+        browser = webdriver.Chrome()
     else:
         raise RuntimeError(f'Unsupported browser: "{browser_name}"')
     browser.maximize_window()
     return browser
 
 
-@pytest.fixture(scope='session', params=['chrome', 'firefox'])
+@pytest.fixture(scope="session", params=["chrome", "firefox"])
 def all_drivers(config, request):
-    url = config['url']
+    url = config["url"]
     browser = get_driver(request.param)
     browser.get(url)
     yield browser
