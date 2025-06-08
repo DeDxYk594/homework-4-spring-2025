bce6c91b35981ba040ca9dcd78fd4e60c5207e75
DeDxYk594 <zhugeo@mail.ru>
2025-06-08 12:15:20 +0300

    some infra changes

--- dump_session.py
+++ dump_session.py
@@ -1,12 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import pathlib
+
+SCRIPT_DIRECTORY = pathlib.Path().absolute()
 
 options = Options()
 
 options.add_argument("--verbose")
-options.add_argument("user-data-dir=user_data_dir")
+options.add_argument(f"user-data-dir={SCRIPT_DIRECTORY}/user_data_dir")
 
 driver = webdriver.Chrome(options=options)
+
+driver.get("https://ads.vk.com")
 
 input("click Enter to exit...")
 driver.quit()
--- hw/code/ui/tests/test_3_create_group.py
+++ hw/code/ui/tests/test_3_create_group.py
@@ -12,7 +12,6 @@ def random_test_number():
 def test_create_group(driver: WebDriver):
     driver.get("https://ads.vk.com/hq/dashboard/ad_plans")
     page = EntityDashboardPage(driver)
-    page.is_opened()
     plan_editing_page = page.go_to_plan_editing()
     group_editing_page = plan_editing_page.go_to_group_editing()
     new_title = f"Новое название {random_test_number()}"
