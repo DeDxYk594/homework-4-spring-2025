a5c784e26a3dc0415a968525bf09722453e3f003
Konstantin <kostya.safronov@gmail.com>
2025-06-18 03:22:20 +0300

    Систематезированы локаторы на тест 5, внесены небольшие правки

--- hw/code/ui/locators/ad_creation_page_locators.py
+++ hw/code/ui/locators/ad_creation_page_locators.py
@@ -2,83 +2,30 @@ from selenium.webdriver.common.by import By
 
 
 class AdCreationPageLocator:
-    TITLE_INPUT = (By.CSS_SELECTOR, "input.vkuiInput__el")
+    # Общие локаторы
     SITE_OPTION = (
         By.XPATH,
         "//span[text()='Сайт']/ancestor::div[contains(@class, 'Option_item__')]",
     )
-
     SITE_INPUT = (By.CSS_SELECTOR, "input[data-testid='site-select-input']")
-
     BUDGET_INPUT = (By.XPATH, "//input[@placeholder='Не задан' and @inputmode='decimal']")
-
-    
-    BUTTON_BY_TEXT = lambda text: (By.XPATH, f"//button[.//span[text()='{text}']]")
     CONTINUE_BUTTON = (By.CSS_SELECTOR, "button[data-testid='next-step-button']")
 
-    AD_PLACEMENT_BUTTONS = (
-        By.CSS_SELECTOR,
-        "button[data-testid='ad-placement-button']",
-    )
-
-    PREVIEW_TITLE = (By.CSS_SELECTOR, "div[data-testid='ad-preview'] h3")
-
-    NATIVE_BLOCK_PREVIEW_TITLE = (
-        By.CSS_SELECTOR,
-        "div[data-testid='ad-preview'] h3"
-    )
-
-    PREVIEW_VIDEO = (
-        By.CSS_SELECTOR,
-        "div[data-testid='ad-preview'] video"
-    )
-
-
+    # Регион
     REGION_MOSCOW_BUTTON = (
         By.CSS_SELECTOR,
         "button[data-testid='region-quick-selection-5506']",
     )
 
+    # Заголовок генерация
     TITLE_GENERATE_ICON = (
         By.XPATH,
         "//button[contains(@class, 'ContentGeneratorButton_magicIconBtn')]",
     )
-
     GENERATED_TITLE_TEXTAREA = (By.CSS_SELECTOR, "textarea.vkuiTextarea__el")
-
     APPLY_GENERATED_TITLE_BUTTON = (By.XPATH, "//button[.//span[text()='Применить']]")
 
-    LOGO_SELECT = (
-        By.XPATH,
-        "//span[@class='vkuiButton__content']//span[text()='Выбрать логотип']"
-    )
-
-    IMAGE_SELECT = (By.XPATH, "//div[contains(@class, 'ImageItem_image')]")
-    IMAGE_ITEM = (By.CSS_SELECTOR, "div[data-testid='image-item']")
-    CHOOSE_LOGO_BUTTON = (By.CSS_SELECTOR, "button[data-testid='set-global-image']")
-    PHOTO_STOCK_TAB = (By.CSS_SELECTOR, "div[data-id='photo_stock_and_generated']")
-    GENERATED_LOGO_ITEM = (By.CSS_SELECTOR, "div[data-testid='image-media-item-loaded']")
-    SECOND_MEDIA_OPTION_LOCATOR = (By.CSS_SELECTOR, "div[data-testid='image-item']")
-    PHOTO_STOCK_IMAGE_LOCATOR = (By.CLASS_NAME, "PhotoStockImagesPreview_itemContent__DoHxc")
-
-
-    MEDIA_BUTTON = (
-        By.XPATH,
-        "//div[starts-with(@class, 'PhotoStockMediaFileSelector_section__')]",
-    )
-
-    ADD_IMAGES_BUTTON = (
-        By.XPATH,
-        "//button[@data-testid='submit' and contains(.//span, 'Добавить')]",
-    )
-    CHANGE_IMAGES_BUTTON = (
-        By.XPATH,
-        "//button[@data-testid='change-image' and contains(.//span, 'Заменить')]",
-    )
-    SAVE_DRAFTS_BUTTON = (
-        By.XPATH,
-        "//button[.//span[contains(text(), 'Сохранить черновик')]]",
-    )
+    # Редактирования объявления
     HEADER_INPUT = (
         By.XPATH,
         "//div[contains(@class, 'EditableTextField__wrapper')]"
@@ -93,17 +40,51 @@ class AdCreationPageLocator:
         By.XPATH,
         "//*[@data-name='textblock:::text_90']//*[@role='alert']",
     )
-    IMAGES_SELECT = (
+    TITLE_ERROR_MESSAGE = (
         By.XPATH,
-        "div[contains(@class, 'ImageItem_image') and contains(@style, 'background-image: url')]",
-    )
-    TO_PUBLISH_BUTTON = (
-        By.XPATH,
-        "//span[@class='vkuiButton__in'][.//span[@class='vkuiButton__content' and text()='Опубликовать']]",
-    )
-    SECOND_MEDIA_IMAGE = (
-        By.CSS_SELECTOR,
-        "div[data-testid='image-item']"
+        "//*[@data-name='textblock:::title_40']//*[@role='alert']",
     )
 
+    # Превью
+    PREVIEW_TITLE = (By.CSS_SELECTOR, "div[data-testid='ad-preview'] h3")
+    PREVIEW_VIDEO = (By.CSS_SELECTOR, "div[data-testid='ad-preview'] video")
+    PREVIEW_STREAM = (By.CSS_SELECTOR, "div[data-testid='ad-preview'] iframe")
+
+    # Кнопка "Опубликовать"
     PUBLISH_BUTTON = (By.CSS_SELECTOR, "button[data-testid='submit-button']")
+
+    # Кнопка выбора логотипа и изображения
+    CHOOSE_LOGO_BUTTON = (By.CSS_SELECTOR, "button[data-testid='set-global-image']")
+    PHOTO_STOCK_TAB = (By.CSS_SELECTOR, "div[data-id='photo_stock_and_generated']")
+    GENERATED_LOGO_ITEM = (By.CSS_SELECTOR, "div[data-testid='image-media-item-loaded']")
+
+    # Выбор изображений
+    IMAGE_ITEM = (By.CSS_SELECTOR, "div[data-testid='image-item']")
+    SECOND_MEDIA_OPTION_LOCATOR = (By.CSS_SELECTOR, "div[data-testid='image-item']")
+    PHOTO_STOCK_IMAGE_LOCATOR = (By.CLASS_NAME, "PhotoStockImagesPreview_itemContent__DoHxc")
+
+    # Выбор фото из генерации
+    IMAGE_SELECT = (By.XPATH, "//div[contains(@class, 'ImageItem_image')]")
+
+    # Кнопки редактирования изображений
+    CHANGE_IMAGES_BUTTON = (
+        By.XPATH,
+        "//button[@data-testid='change-image' and contains(.//span, 'Заменить')]",
+    )
+    ADD_IMAGES_BUTTON = (
+        By.XPATH,
+        "//button[@data-testid='submit' and contains(.//span, 'Добавить')]",
+    )
+    SAVE_DRAFTS_BUTTON = (
+        By.XPATH,
+        "//button[.//span[contains(text(), 'Сохранить черновик')]]",
+    )
+    MEDIA_BUTTON = (
+        By.XPATH,
+        "//div[starts-with(@class, 'PhotoStockMediaFileSelector_section__')]",
+    )
+
+
+
+    def BUTTON_BY_TEXT(text: str):
+        return By.XPATH, f"//button[.//span[text()='{text}']]"
--- hw/code/ui/tests/test_5_create_post.py
+++ hw/code/ui/tests/test_5_create_post.py
@@ -7,14 +7,13 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from ..locators.ad_creation_page_locators import AdCreationPageLocator
 from ..pages.ad_creation_page import AdCreationPage
-from ..locators.ad_creation_page_locators import IMAGE_ITEM
-
 from ..constants import TEST_SITE_URL, TEST_BUDGET, TEST_GENERATED_TITLE, IMAGE_GENERATION_TIMEOUT
+
 
 def test_create_campaign_with_site(driver: WebDriver):
     driver.get("https://ads.vk.com/hq/new_create/ad_plan")
 
-    wait = WebDriverWait(driver, 15)
+    wait = WebDriverWait(driver, IMAGE_GENERATION_TIMEOUT)
 
     # Выбор типа рекламы — "Сайт"
     site_option = wait.until(EC.element_to_be_clickable(AdCreationPageLocator.SITE_OPTION))
@@ -29,6 +28,7 @@ def test_create_campaign_with_site(driver: WebDriver):
     assert continue_btn.is_enabled(), "Кнопка 'Продолжить' неактивна после ввода сайта"
     continue_btn.click()
 
+    # Ввод бюджета
     budget_input = wait.until(EC.visibility_of_element_located(AdCreationPageLocator.BUDGET_INPUT))
     driver.execute_script("arguments[0].scrollIntoView(true);", budget_input)
     budget_input.click()
@@ -40,12 +40,14 @@ def test_create_campaign_with_site(driver: WebDriver):
     assert continue_btn.is_enabled(), "Кнопка 'Продолжить' неактивна после ввода бюджета"
     continue_btn.click()
 
+    # Выбор региона
     moscow_btn = wait.until(EC.element_to_be_clickable(AdCreationPageLocator.REGION_MOSCOW_BUTTON))
     moscow_btn.click()
 
     continue_btn = wait.until(EC.element_to_be_clickable(AdCreationPageLocator.CONTINUE_BUTTON))
     continue_btn.click()
 
+    # Работа с генерацией объявления
     page = AdCreationPage(driver)
 
     page.generate_title(TEST_GENERATED_TITLE)
@@ -53,9 +55,10 @@ def test_create_campaign_with_site(driver: WebDriver):
     page.open_photo_stock_tab()
     page.choose_generated_logo()
 
-    wait.until(lambda d: len(d.find_elements(*IMAGE_ITEM)) > 1)
+    # Ждём, пока появится хотя бы 2 изображения
+    wait.until(lambda d: len(d.find_elements(*AdCreationPageLocator.IMAGE_ITEM)) > 1)
 
-
-    page.select_second_stock_image()
+ 
     page.select_second_media_option()
+
     page.click_publish()
